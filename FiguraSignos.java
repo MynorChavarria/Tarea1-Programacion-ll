7a3882df19f2541975cffce9bb7adc47c98136f0
Mynor Chavarría <52892686+MynorChavarria@users.noreply.github.com>
2019-07-16 22:06:46 -0600

    Add files via upload

---
+++ FiguraSignos.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figura.signos;
+
+/**
+ *
+ * @author max jutiapa
+ */
+public class FiguraSignos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("       Z  "+"       Z");       
+    System.out.println("       ZZ  "+"       ZZ ");      
+    System.out.println("     ZZ  "+"     ZZ");    
+    System.out.println("      ZZZ"+"      ZZZ");  
+    System.out.println("   ZZZ"+"      ZZZ");
+    
+    System.out.println("______________________");
+    System.out.println("**********************");
+    System.out.println("**********************"+"]]]]");
+    System.out.println("**********************"+"]   ]");
+    System.out.println(" ********************"+"[   ]");
+    System.out.println("  ******************"+"[_._] ");
+    System.out.println("   ****************");
+    System.out.println("    **************");
+     System.out.println("    ************" );
+    System.out.println("========================");  
+    System.out.println("========================\n\n\n");
+    
+    
+    
+    
+    
+        System.out.println("   :v:v:v        :v:v:v:v               :v:v:v:v");  
+        System.out.println("   :v:v:v        :v:v:v:v               :v:v:v:v");
+        System.out.println("   :v:v:v         :v:v:v:v             :v:v:v:v"); 
+        System.out.println("                   :v:v:v:v           :v:v:v:v");  
+        System.out.println("                    :v:v:v:v         :v:v:v:v");
+        System.out.println("                      :v:v:v:        :v:v:v");
+        System.out.println("                        :v:v:v      :v:v:v");  
+        System.out.println("                         :v:v:v    :v:v:v");
+        System.out.println("                          :v:v:v  :v:v:v"); 
+        System.out.println("   :v:v:v                   :v:v  :v:v");  
+        System.out.println("   :v:v:v                    :v:v:v:v");
+        System.out.println("   :v:v:v                     :v:v:v"); 
+      
+
+    }
+    
+}
